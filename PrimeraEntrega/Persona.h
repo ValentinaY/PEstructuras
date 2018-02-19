59454e0856b9705e83ec51116723f2228f5dbce4
Leo <dalgomz@hotmail.com>
2018-02-18 22:32:43 -0500

    Carga y Registro de personas

---
+++ PrimeraEntrega/Persona.h
@@ -0,0 +1,47 @@
+#ifndef Persona_H
+#define Persona_H
+
+#include <string>
+
+using namespace std;
+
+class Persona{
+private:
+	string nombre;
+	string apellido;
+	string id;
+	string direccion;
+	string ciudad;
+	string telefono;
+public:
+	/* Los getters no se implementan aparte http://www.learncpp.com/cpp-tutorial/89-class-code-and-header-files/
+	*/
+	string getNombre(){
+		return nombre;
+	}
+	void setNombre(string nname);
+	string getId(){
+		return id;
+	}string getApellido(){
+		return apellido;
+	}
+	void setApellido(string nname);
+	
+	void setId(string nidnum);
+	string getDireccion(){
+		return direccion;
+	}
+	void setDireccion(string naddress);
+	string getCiudad(){
+		return ciudad;
+	}
+	void setCiudad(string ncity);
+	string getTelefono(){
+		return telefono;
+	}
+	void setTelefono(string nphone);
+};
+
+#include "Persona.hxx"
+
+#endif // Persona_H
